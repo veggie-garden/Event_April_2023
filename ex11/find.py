28dbc904f8afebc5f82aeb6bab43589817a166ab
veggie-garden <devcseo@gmail.com>
2023-04-04 15:00:49 +0900

    submit

--- ex11/find.py
+++ ex11/find.py
@@ -1,11 +1,5 @@
 import requests
 
-# Enter the name of a Pokemon: Pikachu
-# Name: Pikachu
-# Abilities:
-# - Static
-# - Lightning-rod
-
 str = input("Enter the name of a Pokemon: ")
 res = requests.get("https://pokeapi.co/api/v2/pokemon/" + str.lower())
 print("Name: " + str)
---
+++ ex14/run.sh
@@ -0,0 +1,1 @@
+./sastantua_macos $@
